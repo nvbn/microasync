331bcf9d2988e1a9e1a530d96e8c37d4619d24f8
nvbn <nvbn.rm@gmail.com>
2014-10-24 00:12:00 +0400

    Fix package

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(name='microasync',
-      version='0.1',
+      version='0.3',
       description='Green threads and CSP for micropython.',
       long_description=open('README.rst').read(),
       url='https://github.com/nvbn/microasync/',
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
       license='MIT',
-      py_modules=['microasync'])
+      packages=['microasync'])
